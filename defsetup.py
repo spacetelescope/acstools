5d7584ee9d54666fd934b634acb176edb9ee9a36
hack <hack@stsci.edu>
2010-09-30 15:42:09 +0000

    A new version of 'csc_kill' was provided by Norman Grogin. This version was renamed to 'acs_destripe' and used to replace the current version of the task 'csc_kill' in the 'acstools' package. The help file, function and module docstrings and the 'doc/' directory were also updated to generate Sphinx documentation while still providing a useful TEAL helpfile. The 'defsetup.py' file was also updated to correctly install the new OS command-line task interface for the renamed task 'acs_destripe'. WJH
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/acstools@10434 fe389314-cf27-0410-b35b-8c050e845b92

--- defsetup.py
+++ defsetup.py
@@ -3,13 +3,13 @@ from __future__ import division # confidence high
 pkg =  "acstools"
 
 setupargs = {
-    'version' : 		"1.0.1",
+    'version' : 		"1.0.2",
     'description' :	    "Python Tools for ACS Data",
-    'author' : 		    "Warren Hack",
+    'author' : 		    "Warren Hack, Norman Grogin",
     'author_email' : 	"help@stsci.edu",
     'license' : 		"http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
     'data_files' :      [( pkg+"/pars", ['lib/pars/*']),( pkg, ['lib/*.help'])],
-    'scripts' :         [ 'lib/csc_kill', 'lib/updatenpol'] ,
+    'scripts' :         [ 'lib/acs_destripe', 'lib/updatenpol'] ,
     'platforms' : 	    ["Linux","Solaris","Mac OS X","Win"],
 }
 
