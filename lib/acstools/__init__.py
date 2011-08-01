a6629ef750fc05be20aa26332da787b42d3cd0e2
sienkiew <sienkiew@stsci.edu>
2011-08-01 21:40:53 +0000

    static version numbers in __init__
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/acstools@13441 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/acstools/__init__.py
+++ lib/acstools/__init__.py
@@ -8,16 +8,21 @@ module.
 
 """
 
-__version__ = ''
-__svn_version__ = 'Unable to determine SVN revision'
-__full_svn_info__ = ''
-__setup_datetime__ = None
+if False :
+    __version__ = ''
 
-try:
-    __version__ = __import__('pkg_resources').\
-                        get_distribution('acstools').version
-except:
-    pass
+    __svn_version__ = 'Unable to determine SVN revision'
+    __full_svn_info__ = ''
+    __setup_datetime__ = None
+
+    try:
+        __version__ = __import__('pkg_resources').\
+                            get_distribution('acstools').version
+    except:
+        pass
+
+else :
+    __version__ = '1.4.0'
 
 try:
     from acstools.svninfo import (__svn_version__, __full_svn_info__,
