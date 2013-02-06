f1483cf180d9b96fbeb7c7780e73abc43e3cb142
sienkiew <sienkiew@stsci.edu>
2013-02-06 20:25:45 +0000

    get version from new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/acstools/trunk@22255 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/acstools/__init__.py
+++ lib/acstools/__init__.py
@@ -8,27 +8,7 @@ module.
 
 """
 
-if False :
-    __version__ = ''
-
-    __svn_version__ = 'Unable to determine SVN revision'
-    __full_svn_info__ = ''
-    __setup_datetime__ = None
-
-    try:
-        __version__ = __import__('pkg_resources').\
-                            get_distribution('acstools').version
-    except:
-        pass
-
-else :
-    __version__ = '1.7.1'
-
-try:
-    from acstools.svninfo import (__svn_version__, __full_svn_info__,
-                                  __setup_datetime__)
-except ImportError:
-    pass
+from .version import *
 
 import acs_destripe
 import PixCteCorr
