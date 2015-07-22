dc8fad074b6237b9b48ebc7eb72579e7b7dd7083
lim <lim@stsci.edu>
2015-07-22 14:20:49 +0000

    Moved the print statement so it will not interfere with auto doc build listing
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/acstools/trunk@42300 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/acstools/__init__.py
+++ lib/acstools/__init__.py
@@ -5,7 +5,6 @@ These tasks include:
 Utility and library functions used by these tasks are also included in this
 module.
 
-
 """
 from __future__ import absolute_import, print_function
 from .version import *
@@ -20,11 +19,11 @@ from . import acs2d
 from . import acsrej
 from . import acssum
 
-# We can remove this in the future when people no longer care about PixCteCorr
-print("\nPixCteCorr is no longer supported. Please use acscte.\n")
-
 # These lines allow TEAL to print out the names of TEAL-enabled tasks
 # upon importing this package.
 import os
 from stsci.tools import teal
 teal.print_tasknames(__name__, os.path.dirname(__file__))
+
+# We can remove this in the future when people no longer care about PixCteCorr
+print("PixCteCorr is no longer supported. Please use acscte.")
