ac951612ba1f0af499d63f3367f6943949d07c3b
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-24 15:27:25 -0400

    Implement RELIC

--- setup.py
+++ setup.py
@@ -1,15 +1,71 @@
 #!/usr/bin/env python
+import os
+import subprocess
+import sys
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+if os.path.exists('relic'):
+    sys.path.insert(1, 'relic')
+    import relic.release
+else:
+    try:
+        import relic.release
+    except ImportError:
+        try:
+            subprocess.check_call(['git', 'clone', 
+                'https://github.com/jhunkeler/relic.git'])
+            sys.path.insert(1, 'relic')
+            import relic.release
+        except subprocess.CalledProcessError as e:
+            print(e)
+            exit(1)
+
+
+version = relic.release.get_info()
+relic.release.write_template(version, 'lib/acstools')
 
 setup(
-    setup_requires=['d2to1>=0.2.9', 'stsci.distutils>=0.3.2'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'acstools',
+    version = version.pep386,
+    author = 'Matt Davis, Warren Hack, Norman Grogin, Pey Lian Lim, Sara Ogaz, Leornado Ubeda, Mihai Cara, David Borncamp',
+    author_email = 'help@stsci.edu',
+    description = 'Python Tools for ACS (Advanced Camera for Surveys) Data',
+    url = 'https://github.com/spacetelescope/acstools',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'astropy',
+        'nose',
+        'numpy',
+        'scikit-image',
+        'scipy',
+        'sphinx',
+        'stsci.imagestats',
+        'stsci.sphinxext',
+        'stsci.tools',
+    ],
+
+    package_dir = {
+        '': 'lib'
+    },
+    packages = find_packages('lib'),
+    package_data = {
+        'acstools': [
+            'pars/*',
+            'LICENSE.txt'
+        ]
+    },
+    entry_points = {
+        'console_scripts': [
+            'acs_destripe=acstools.acs_destripe:main',
+            'acs_destripe_plus=acstools.acs_destripe_plus:main'
+        ],
+    },
 )
