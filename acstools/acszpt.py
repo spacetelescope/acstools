d13f219a1a9f183e742ca1ca3362f0b997378bb7
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-01-25 20:45:45 -0500

    Review edits

--- acstools/acszpt.py
+++ acstools/acszpt.py
@@ -1,16 +1,18 @@
 """
-The acszpt module contains a class, Query(), that was implemented to provide
-users with a means to programmatically query the ACS Zeropoints Calculator hosted
-at https://acszeropoints.stsci.edu. The API works by submitting requests to the
+This module contains a class, :class:`Query`, that was implemented to provide
+users with means to programmatically query the
+`ACS Zeropoints Calculator <https://acszeropoints.stsci.edu>`_.
+The API works by submitting requests to the
 ACS Zeropoints Calculator referenced above and hence, it is only valid for ACS
 specific instruments (HRC, SBC, or WFC).
 
-The API can be used in two ways by specifying either a (date, detector, filter)
-combination or just a (date, detector) combination. In the first case, the query
+The API can be used in two ways by specifying either a
+``(date, detector, filter)`` combination or just a ``(date, detector)``
+combination. In the first case, the query
 will return the zeropoint information for the specific filter and detector at
 specified date. In the second case, the query will return the zeropoint
 information for all the filters for the desired detector at the specified date.
-In either case, the result will be an Astropy table where each column has the
+In either case, the result will be an Astropy table, where each column has the
 correct units attached.
 
 Examples
@@ -19,7 +21,7 @@ Retrieve the zeropoint information for all the filters on 2016-04-01 for WFC:
 
 >>> from acstools import acszpt
 >>> date = '2016-04-01'
->>> detector='WFC'
+>>> detector = 'WFC'
 >>> q = acszpt.Query(date=date, detector=detector)
 >>> zpt_table = q.fetch()
 >>> print(zpt_table)
@@ -45,7 +47,7 @@ Retrieve the zeropoint information for the F435W filter on 2016-04-01 for WFC:
 
 >>> from acstools import acszpt
 >>> date = '2016-04-01'
->>> detector='WFC'
+>>> detector = 'WFC'
 >>> filt = 'F435W'
 >>> q = acszpt.Query(date=date, detector=detector, filter=filt)
 >>> zpt_table = q.fetch()
@@ -54,70 +56,69 @@ FILTER PHOTPLAM        PHOTFLAM         STmag  VEGAmag  ABmag
        Angstrom erg / (Angstrom cm2 s) mag(ST)   mag   mag(AB)
 ------ -------- ---------------------- ------- ------- -------
  F435W   4329.2              3.148e-19  25.155  25.763  25.665
-"""
 
-from collections import OrderedDict
+"""
+from six.moves.urllib.request import urlopen
+from six.moves.urllib.error import URLError
+
 import datetime as dt
 import logging
-from urllib import request, error as request_errors
+from collections import OrderedDict
 
-from astropy.table import Table
 import astropy.units as u
+from astropy.table import Table
 from bs4 import BeautifulSoup
 
 __taskname__ = "acszpt"
-__version__ = "1.0"
 __author__ = "Nathan Miles"
+__version__ = "1.0"
 __vdate__ = "22-Jan-2019"
+
 __all__ = ['Query']
 
 # Initialize the logger
 logging.basicConfig()
-LOG = logging.getLogger('{}.{}'.format(__taskname__,__all__[0]))
+LOG = logging.getLogger('{}.{}'.format(__taskname__, 'Query'))
 LOG.setLevel(logging.INFO)
 
 
 class Query(object):
-    """ Class used to interface with the ACS Zeropoints Calculator API.
+    """Class used to interface with the ACS Zeropoints Calculator API.
 
     Parameters
     ----------
     date : str
         Input date in the following ISO format, YYYY-MM-DD.
+
     detector : str
         One of the three channels on ACS: HRC, SBC, or WFC.
-    filt : str, optional
+
+    filt : str or `None`, optional
         One of valid filters for the chosen detector. If no filter is supplied,
-        all of the filters for the chosen detector will be used.
+        all of the filters for the chosen detector will be used:
 
-            * HRC:
-                 F220W, F250W, F330W,
-                 F344N, F435W, F475W,
-                 F502N, F550M, F555W,
-                 F606W, F625W, F658N, F660N,
-                 F775W, F814W, F850LP, F892N
-
-            * WFC:
-                 F435W, F475W,
-                 F502N, F550M, F555W,
-                 F606W, F625W, F658N, F660N,
-                 F775W, F814W, F850LP, F892N
-            * SBC:
-                F115LP, F122M, F125LP,
-                F140LP, F150LP, F165LP
+            * HRC: F220W, F250W, F330W, F344N, F435W, F475W,
+              F502N, F550M, F555W, F606W, F625W, F658N, F660N,
+              F775W, F814W, F850LP, F892N
+            * WFC: F435W, F475W, F502N, F550M, F555W,
+              F606W, F625W, F658N, F660N, F775W, F814W, F850LP, F892N
+            * SBC: F115LP, F122M, F125LP, F140LP, F150LP, F165LP
 
     Attributes
     ----------
     date : str
-        **Attribute**, the user supplied date.
-    detector : str
-        **Attribute**, the user supplied detector.
-    filt : str, None
-        **Attribute**, the user supplied filter if one was given.
-    zpt_table : astropy.table.Table
-        **Attribute**, the results returned by the ACS Zeropoint Calculator
-    """
+        The user supplied date.
 
+    detector : str
+        The user supplied detector.
+
+    filt : str or `None`
+        The user supplied filter, if one was given.
+
+    zpt_table : `astropy.table.Table`
+        The results returned by the ACS Zeropoint Calculator.
+
+    """
     def __init__(self, date, detector, filt=None):
 
         # Set the public attributes
@@ -154,85 +155,72 @@ class Query(object):
 
         self._zpts = OrderedDict()
         self._acs_installation_date = dt.datetime(2002, 3, 7)
+        self._msg_div = '-' * 79
         self._valid_detectors = ['HRC', 'SBC', 'WFC']
         self._response = None
         self._failed = False
 
     def _check_inputs(self):
-        """ Check the inputs to ensure they are valid
+        """Check the inputs to ensure they are valid.
 
         Returns
         -------
-        Bool
+        status : bool
             True if all inputs are valid, False if one is not.
-        """
 
-        valid_date = True
-        valid_detector = True
-        valid_filter = True
+        """
         # Determine the submitted detector is valid
         if self.detector not in self._valid_detectors:
-            LOG.error(' {} is not a valid detector option.'.
-                  format(self.detector))
-            msg = ' Please choose one of the following:\n'
-            for val in self._valid_detectors:
-                msg += '{}\n'.format(val)
-
-            LOG.info(msg)
-            print('-'*79)
-            valid_detector = False
+            msg = ('{} is not a valid detector option.\n'
+                   'Please choose one of the following:\n{}\n'
+                   '{}'.format(self.detector,
+                               '\n'.join(self._valid_detectors),
+                               self._msg_div))
+            LOG.error(msg)
+            return False
 
         # Determine if the submitted filter is valid
-        if self.filt is None:
-            pass
-        elif valid_detector and self.filt not in self.valid_filters[self.detector]:
-            valid_filter = False
-            LOG.error(' {} is not a valid filter for {}'.
-                  format(self.filt, self.detector))
-            msg = ' Please choose one of the following:\n'
-
-            for filt in self.valid_filters[self.detector]:
-                msg += '{}\n'.format(filt)
-            LOG.info(msg)
-            print('-' * 79)
+        if (self.filt is not None and
+                self.filt not in self.valid_filters[self.detector]):
+            msg = ('{} is not a valid filter for {}\n'
+                   'Please choose one of the following:\n{}\n'
+                   '{}'.format(self.filt, self.detector,
+                               '\n'.join(self.valid_filters[self.detector]),
+                               self._msg_div))
+            LOG.error(msg)
+            return False
 
         # Determine if the submitted date is valid
         date_check = self._check_date()
         if date_check is not None:
-            valid_date = False
-            LOG.error(' {}'.format(date_check))
-            print('-'*79)
-
-        # Only continute if all the inputs are valid options
-        if all([valid_date, valid_filter, valid_detector]):
-            return True
-        else:
+            LOG.error('{}\n{}'.format(date_check, self._msg_div))
             return False
 
+        return True
+
     def _check_date(self, fmt='%Y-%m-%d'):
-        """Convenience method for determining if the input date is valid
+        """Convenience method for determining if the input date is valid.
 
         Parameters
         ----------
         fmt : str
-            The format of the date string. The default is %Y-%m-%d, which
-            corresponds to YYYY-MM-DD.
+            The format of the date string. The default is ``%Y-%m-%d``, which
+            corresponds to ``YYYY-MM-DD``.
 
         Returns
         -------
-        None, str
-            If the date is valid, returns None. If the date is invalid, returns
-            a message explaining the issue
+        status : str or `None`
+            If the date is valid, returns `None`. If the date is invalid,
+            returns a message explaining the issue.
+
         """
         result = None
         try:
             dt_obj = dt.datetime.strptime(self.date, fmt)
         except ValueError:
-            result =  'Date format does not match YYYY-MM-DD'
+            result = '{} does not match YYYY-MM-DD format'.format(self.date)
         else:
-            if self._acs_installation_date < dt_obj:
-                pass
-            else:
+            if dt_obj < self._acs_installation_date:
                 result = ('The observation cannot occur '
                           'before ACS was installed ({})'.
                           format(self._acs_installation_date.strftime(fmt)))
@@ -240,32 +228,35 @@ class Query(object):
             return result
 
     def _submit_request(self):
-        """ Submit a request to the ACS Zeropoint Calculator
+        """Submit a request to the ACS Zeropoint Calculator.
 
-        If an exception is raised during the request an error message is
-        printed. Otherwise, the response is saved in the corresponding
+        If an exception is raised during the request, an error message is
+        given. Otherwise, the response is saved in the corresponding
         attribute.
+
         """
         try:
-            self._response = request.urlopen(self._url)
-        except request_errors.URLError as e:
-            LOG.error(e)
-            print('-'*79)
-            LOG.info('The query failed! Check your input args and if the error \n'
-                  'persists submit a ticket to the ACS Help Desk at '
-                  'hsthelp.stsci.edu with the error message displayed above.')
+            self._response = urlopen(self._url)
+        except URLError as e:
+            msg = ('{}\n{}\nThe query failed! Please check your inputs. '
+                   'If the error persists, submit a ticket to the '
+                   'ACS Help Desk at hsthelp.stsci.edu with the error message '
+                   'displayed above.'.format(str(e), self._msg_div))
+            LOG.error(msg)
             self._failed = True
+        else:
+            self._failed = False
 
     def _parse_response(self):
-        """ Parse the html response returned from the HTML request to find all
+        """Parse the HTML response returned from the request to find all
         tables on the webpage.
 
-        Using bs4, find all the <tb> </tb> tags present in the response. Save
-        the results in an OrderedDict() so when parsing occurs we get stable
-        results.
+        Using ``beautifulsoup4``, find all the ``<tb> </tb>`` tags present in
+        the response. Save
+        the results in an :py:class:`OrderedDict` so when parsing occurs,
+        we get stable results.
 
         """
-
         soup = BeautifulSoup(self._response.read(), 'html.parser')
 
         # Grab all elements in the table returned by the ZPT calc.
@@ -276,16 +267,17 @@ class Query(object):
         # the html table. The first 6 elements in the list of all elements
         # correspond to the column names. The values after that correspond to
         # actual data for each row
-        for j in range(6):
-            for i, val in enumerate(td[j::6]):
+        BLOCK_SIZE = 6
+        for j in range(BLOCK_SIZE):
+            for i, val in enumerate(td[j::BLOCK_SIZE]):
                 if not i:
                     self._zpts[val.text.split('[')[0].strip()] = []
                 else:
                     self._zpts[list(self._zpts.keys())[j]].append(val.text)
 
     def _format_results(self):
-        """ Format the results into an astropy.table.Table with corresponding
-        units and assign it to the zpt_table attribute
+        """Format the results into an `astropy.table.Table` with corresponding
+        units and assign it to the ``zpt_table`` attribute.
 
         """
         data_types = []
@@ -325,31 +317,32 @@ class Query(object):
         self.zpt_table = tab
 
     def fetch(self):
-        """ **Method**, used to submit the request to the ACS Zeropoints Calculator
+        """Submit the request to the ACS Zeropoints Calculator.
 
-        This the method will:
+        This method will:
 
-         - submit the request
-         - parse the response
-         - format the results into a table with the correct units
+        * submit the request
+        * parse the response
+        * format the results into a table with the correct units
 
         Returns
         -------
-        astropy.table.Table or None
-            If the request was successful returns a table, otherwise, None
+        tab : `astropy.table.Table` or `None`
+            If the request was successful, returns a table; otherwise, `None`.
+
         """
-        LOG.info(' Checking inputs...')
+        LOG.info('Checking inputs...')
         valid_inputs = self._check_inputs()
+
         if valid_inputs:
-            LOG.info(' Submitting request to {}'.format(self._url))
+            LOG.info('Submitting request to {}'.format(self._url))
             self._submit_request()
             if self._failed:
-                return None
-            else:
-                LOG.info(' Parsing the response and formatting the results...')
-                self._parse_response()
-                self._format_results()
-                return self.zpt_table
-        else:
-            LOG.error('Please fix the incorrect input(s)')
-            return None
+                return
+
+            LOG.info('Parsing the response and formatting the results...')
+            self._parse_response()
+            self._format_results()
+            return self.zpt_table
+
+        LOG.error('Please fix the incorrect input(s)')
