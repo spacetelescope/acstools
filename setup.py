a5a5d1984558880224d35ba82935e30527ee7384
Matt Rendina <mrendina@stsci.edu>
2020-09-11 13:15:40 -0400

    Add project URLs to package metadata

--- setup.py
+++ setup.py
@@ -34,4 +34,11 @@ if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
-setup(use_scm_version={'write_to': 'acstools/version.py'})
+setup(
+    use_scm_version={'write_to': 'acstools/version.py'},
+    project_urls={
+        'Bug Reports': 'https://github.com/spacetelescope/acstools/issues/',
+        'Source': 'https://github.com/spacetelescope/acstools/',
+        'Help': 'https://hsthelp.stsci.edu',
+    },
+)
