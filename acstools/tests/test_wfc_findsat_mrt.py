db982f3b9c76d52fbc39ee70cfdf620e58a10935
David Stark <dstark@stsci.edu>
2023-01-31 21:41:45 -0500

    added comparison between derived mask and truth

--- acstools/tests/test_wfc_findsat_mrt.py
+++ acstools/tests/test_wfc_findsat_mrt.py
@@ -2,11 +2,17 @@
 Test satellite trail detection and masking using findsat_mrt
 '''
 
+from astropy.utils.data import get_pkg_data_filename
 from acstools.findsat_mrt import wfc_wrapper
 from acstools.tests.helpers import BaseACSTOOLS
-import acstools as a
-package_path = a.__path__[0]
 import os
+from astropy.io.fits import FITSDiff
+import logging
+
+
+logging.basicConfig()
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
 
 #update all this with my code
 
@@ -30,9 +36,22 @@ class TestFindsatMRT(BaseACSTOOLS):
                     threads=8, execute=True, save_mask=True,
                     save_diagnostic=False, save_catalog=True)
 
-        #maskfile = rootname + 'flc_mask.fits'
-        #catalogfile = rootname + 'flc_catalog.fits'
 
-        # Compare results.
+        #Compare mask with truth
+        creature_report = ''
+        all_okay = True
+        desired = tmpdir + rootname + '_flc_mask_ref.fits'
+        truthpath = get_pkg_data_filename(
+                    os.path.join('data', 'truth', desired),
+                    package='acstools.tests',
+                    show_progress=False, remote_timeout=self.timeout)
+        fdiff = FITSDiff(inputfile, desiredpath, rtol=rtol, atol=atol,
+                         ignore_keywords=ignore_keywords)
+        creature_report += fdiff.report()
+        
+        if not all_okay and raise_error:
+            raise AssertionError(os.linesep + creature_report)
+            
         #self.compare_outputs([(maskfile, truthmaskfile),
         #                      (catalogfile, truthcatalogfile)])
+
