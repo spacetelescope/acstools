120596bc5c84f8b04bcfe6923726044579b8caf3
David Stark <dstark@stsci.edu>
2023-01-31 22:17:08 -0500

    fixed error in fits file comparison

--- acstools/tests/test_wfc_findsat_mrt.py
+++ acstools/tests/test_wfc_findsat_mrt.py
@@ -31,8 +31,8 @@ class TestFindsatMRT(BaseACSTOOLS):
         self.get_input_file(inputfile, skip_ref=True)
 
         wfc_wrapper(inputfile, binsize=4, extension=4,
-                    output_root='jc8m32j5q',
-                    output_dir = tmpdir,
+                    output_root='jc8m32j5q_flc',
+                    output_dir = tmpdir + '/',
                     threads=8, execute=True, save_mask=True,
                     save_diagnostic=False, save_catalog=True)
 
@@ -40,14 +40,19 @@ class TestFindsatMRT(BaseACSTOOLS):
         #Compare mask with truth
         creature_report = ''
         all_okay = True
-        desired = tmpdir + rootname + '_flc_mask_ref.fits'
-        truthpath = get_pkg_data_filename(
+        actual = '{}/{}_flc_mask.fits'.format(tmpdir,rootname)
+        desired = rootname + '_flc_mask_ref.fits'
+        LOG.info('actual: {}'.format(actual))
+        desiredpath = get_pkg_data_filename(
                     os.path.join('data', 'truth', desired),
                     package='acstools.tests',
                     show_progress=False, remote_timeout=self.timeout)
-        fdiff = FITSDiff(inputfile, desiredpath, rtol=rtol, atol=atol,
-                         ignore_keywords=ignore_keywords)
+        LOG.info('desiredpath: {}'.format(desiredpath))
+        fdiff = FITSDiff(actual, desiredpath, rtol=1e-7, atol=0)
+        LOG.info('fdiff = {}'.format(fdiff))
         creature_report += fdiff.report()
+        
+        LOG.info(creature_report)
         
         if not all_okay and raise_error:
             raise AssertionError(os.linesep + creature_report)
