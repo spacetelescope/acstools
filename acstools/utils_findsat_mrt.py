31d1ead43f759c76f16b77a6feb845e46246c251
David Stark <dstark@stsci.edu>
2024-02-28 14:35:41 -0500

    update_dq routine can now rescale input mask dimensions

--- acstools/utils_findsat_mrt.py
+++ acstools/utils_findsat_mrt.py
@@ -10,7 +10,7 @@ import numpy as np
 from astropy.convolution import convolve, Gaussian2DKernel
 from astropy.io import fits
 from astropy.modeling import models, fitting
-from astropy.nddata import Cutout2D
+from astropy.nddata import Cutout2D, block_replicate
 from astropy.stats import sigma_clip
 from astropy.table import Table, vstack
 from astropy.utils.exceptions import AstropyUserWarning
@@ -1179,7 +1179,7 @@ def radon(image, theta=None, circle=False, *, preserve_range=False,
         return radon_image
 
 
-def update_dq(filename, ext, mask, dqval=16384, verbose=True):
+def update_dq(filename, ext, mask, dqval=16384, verbose=True, expand_mask=False):
     """Update the given image and DQ extension with the given
     satellite trails mask and flag.
 
@@ -1199,10 +1199,48 @@ def update_dq(filename, ext, mask, dqval=16384, verbose=True):
         tailored for ACS/WFC.
     verbose : bool, optional
         Print extra information to the terminal.
+    expand_mask: int, optional
+        Allows the mask to be expanded to match the size of the original mask.
+        This is relevant for masks that were generated used binned versions of
+        the original image
 
     """
     with fits.open(filename, mode='update') as pf:
         dqarr = pf[ext].data
+
+        # check that the old mask and new mask are the same size
+        same_size = mask.shape == dqarr.shape
+
+        if not same_size:
+            LOG.info('Inconsistent mask sizes: \n' 
+                     'Input mask: {} \n' 
+                     'DQ array: {}'.format(mask.shape, dqarr.shape))
+            if not expand_mask:
+                LOG.warning('Cannot proceed due to size mismatch.\n'
+                            'Set expand_mask=True if the mask was generated with \n'
+                            'binned data and needs to be enlarged to the original size')
+                return
+            else:
+                LOG.info('Enlarging mask to original size')
+                factor_x = np.floor(dqarr.shape[1]/mask.shape[1]).astype(int)
+                factor_y = np.floor(dqarr.shape[0]/mask.shape[0]).astype(int)
+                mask = block_replicate(mask, [factor_y,factor_x], conserve_sum=False)
+
+                # for original data that had odd dimensions before binning, the 
+                # mask still will not match the original. Duplicate end rows/columns in this case
+
+                same_size = mask.shape == dqarr.shape
+                if not same_size:
+                    LOG.info('Still inconsistent mask sizes: \n' 
+                             'Input mask: {} \n' 
+                             'DQ array: {}'.format(mask.shape, dqarr.shape))
+                    LOG.info('Duplicating end rows/columns to match original size')
+                    while mask.shape[0] != dqarr.shape[0]:
+                        mask = np.vstack([mask,mask[-1,:]])
+
+                    while mask.shape[1] != dqarr.shape[1]:
+                        mask = np.hstack([mask,mask[:,-1].reshape(mask.shape[0],1)])
+
         old_mask = (dqval & dqarr) != 0  # Existing flagged trails
         new_mask = mask & ~old_mask  # Only flag previously unflagged trails
         npix_updated = np.count_nonzero(new_mask)
