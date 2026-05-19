7bc386dc79be40792cbeaf1720ea79efadb87c46
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-05-19 14:59:36 -0400

    Just ignore PendingSkimage2Change brute force

--- acstools/satdet.py
+++ acstools/satdet.py
@@ -150,23 +150,20 @@ biweight_midvariance = partial(biweight_scale, modify_sample_size=False)
 # OPTIONAL
 try:
     import skimage
-
-    SKIMAGE_LT_0_26 = not minversion(skimage, "0.26.0")
-    SKIMAGE_LT_0_27 = not minversion(skimage, "0.26.1.dev")  # 0.27
-
     from scipy import stats
     from skimage import transform
     from skimage import morphology as morph
     from skimage import exposure
-    if SKIMAGE_LT_0_27:
+    with warnings.catch_warnings():
+        # skimage>=0.27 has some PendingSkimage2Change for canny
+        warnings.simplefilter("ignore")
         from skimage.feature import canny
-    else:
-        from skimage2.feature import canny
 except ImportError:
     HAS_OPDEP = False
     SKIMAGE_LT_0_26 = True
 else:
     HAS_OPDEP = True
+    SKIMAGE_LT_0_26 = not minversion(skimage, "0.26.0")
 
 try:
     import matplotlib.pyplot as plt
